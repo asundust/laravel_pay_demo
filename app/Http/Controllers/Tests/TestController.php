3e2e3b8514eec22cdcee8d5dc3eb9afb500596fa
asundust <asundust@qq.com>
2021-02-03 14:06:15 +0800

    添加微信授权测试

--- app/Http/Controllers/Tests/TestController.php
+++ app/Http/Controllers/Tests/TestController.php
@@ -3,29 +3,37 @@
 namespace App\Http\Controllers\Tests;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\WechatTrait;
 
 class TestController extends Controller
 {
+    use WechatTrait;
+
     /**
      * TestController constructor.
      */
     public function __construct()
     {
-        if (app()->isProduction()) {
+        if (app()->isProduction() && in_array(__FUNCTION__, ['wechatOauthTest'])) {
             abort(404);
         }
     }
 
-    public function t($action = '')
+    public function t($fun = '')
     {
-        if (0 == strlen($action)) {
-            $action = 'a';
+        if (0 == strlen($fun)) {
+            $fun = 'a';
         }
-        $result = $this->$action();
+        $result = $this->$fun();
         if (!empty($result)) {
             return $result;
         }
-        dd('结束运行方法：'.$action);
+        dd('结束运行方法：'.$fun);
+    }
+
+    public function wechatOauthTest()
+    {
+        dd($this->getWechatUser());
     }
 
     // public function a()
